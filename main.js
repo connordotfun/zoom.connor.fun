5def193953d8b146351d4f9c32caedd5fcd724e4
Ethan Hunter <fc.spaceman@gmail.com>
2020-04-06 15:32:47 -0600

    add random chat messages

--- main.js
+++ main.js
@@ -9,8 +9,21 @@ const videos = [
     'https://ak5.picdn.net/shutterstock/videos/1042086325/preview/stock-footage-head-shot-close-up-happy-old-retired-man-looking-at-camera-sitting-at-home-webcam-view-s.webm',
     'https://ak3.picdn.net/shutterstock/videos/1045309183/preview/stock-footage-confident-young-indian-muslim-business-woman-wear-hijab-speak-look-at-camera-islamic-arabic-lady.webm',
     'https://ak9.picdn.net/shutterstock/videos/1039338329/preview/stock-footage-serious-senior-old-grandfather-talking-looking-at-camera-at-home-elder-man-online-teacher-blogger.webm',
-    'https://ak0.picdn.net/shutterstock/videos/1028517230/preview/stock-footage-overjoyed-millennial-vloggers-record-amusing-video-couple-sitting-on-couch-chatting-looking-at-web.webm'
+    'https://ak0.picdn.net/shutterstock/videos/1028517230/preview/stock-footage-overjoyed-millennial-vloggers-record-amusing-video-couple-sitting-on-couch-chatting-looking-at-web.webm',
+    'https://ak9.picdn.net/shutterstock/videos/1039986119/preview/stock-footage-drunk-sleepy-woman-is-vomiting-in-toilet-sitting-on-the-floor-with-wine-bottle-in-hand-at-home.webm',
 ]
+
+var users = null;
+
+function getUsers() {
+    $.ajax({
+        url: 'https://randomuser.me/api/?results=9',
+        dataType: 'json',
+        success: function(data) {
+          users = data.results;
+        }
+      });       
+}
 
 function createVideos() {
     //TODO: select random element to be user's webcam.
@@ -54,4 +67,46 @@ function randomTalking() {
     }
 
     containers[index].style.outline = '2px solid greenyellow';
+}
+
+function newChatMessage() {
+    if (users != null && _.random(5) == 0) { //users loaded and 1/6 chance
+        console.log("new message");
+        $.ajax({
+            url: 'https://connor-dot-fun-slack-off.herokuapp.com/new_pairing',
+            dataType: 'json',
+            success: function(data) {
+                console.log(data);
+                user = users[_.random(8)];
+                firstName = user.name.first;
+                lastName = user.name.last;
+                image = user.picture.thumbnail;
+
+                createChatDom(firstName, lastName, image, data[0].text);
+            }
+        }); 
+    }
+}
+
+function createChatDom(firstName, lastName, image, message) {
+    var container = document.getElementsByClassName("control-side")[0];
+    var chatElement = document.createElement('div');
+    chatElement.setAttribute('class', 'chat-element');
+    var chatImage = document.createElement('img');
+    chatImage.setAttribute('class', 'chat-icon');
+    var chatName = document.createElement('div');
+    chatName.setAttribute('class', 'chat-name');
+    var chatMessage = document.createElement('div');
+    chatMessage.setAttribute('class', 'chat-message');
+
+    chatElement.appendChild(chatImage);
+    chatElement.appendChild(chatName);
+    chatElement.appendChild(chatMessage);
+
+    container.appendChild(chatElement);
+
+    chatMessage.innerText = message;
+    chatName.innerText = firstName + " " + lastName;
+
+    chatImage.setAttribute('src', image);
 }
